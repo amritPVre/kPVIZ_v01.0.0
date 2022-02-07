3d900d1a44329ba26583512cec32371a15a0b095
amritPVre <83923234+amritPVre@users.noreply.github.com>
2022-02-07 23:13:13 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
-$ sudo apt-get install wkhtmltopdf
+
 wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 tar xvf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 mv wkhtmltox/bin/wkhtmlto* /usr/bin/
